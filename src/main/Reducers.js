6ec3bc2657d3f43ef590b30d28fae686fe472999
Rafael Caseiro <rafael_lbp@hotmail.com>
2018-10-09 02:28:53 -0300

    commit

--- src/main/Reducers.js
+++
@@ -1,8 +0,0 @@
-import  {combineReducers} from 'redux'
-import desafioReducers from '../components/desafio/DesafioReducer'
-const rootReducer = combineReducers({
-    desafio: desafioReducers
-
-});
-
-export default rootReducer
